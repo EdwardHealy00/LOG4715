ad487408bc8477863b0a7ffd4c676b9424e0fd62
Simon Mercier <simon-1.mercier@polymtl.ca>
2022-11-10 11:13:16 -0500

    new sandstone

--- TP2/Assets/Scripts/ResetButton.cs
+++ TP2/Assets/Scripts/ResetButton.cs
@@ -8,6 +8,7 @@ public class ResetButton : MonoBehaviour
 {
     private void Awake()
     {
+        Debug.Log("ResetButton.Awake");
         GetComponent<Button>().onClick.AddListener(() => SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex));
     }
 }
